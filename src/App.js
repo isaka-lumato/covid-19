b86b3ba52bc2445b3a695b5d1482432f73a340c2
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2021-11-26 15:28:57 +0300

    functionality and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import CountryData from './components/CountryData';
 
-function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/country/:name" element={<CountryData />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,67 @@
+/* eslint-disable */
+
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { fetchData } from "../redux/covidData/covid";
+
+function Home() {
+  const covidReducer = useSelector((state) => state.covidReducer.covid);
+  const dispatch = useDispatch();
+  let Europe = covidReducer.filter((item) => item.continent === "Europe");
+  const loci = useLocation();
+  const query = new URLSearchParams(loci.search);
+  const buscar = query.get("buscar") || "";
+
+  const navigate = useNavigate();
+  Europe = Europe.filter((country) =>
+    country.country.includes(buscar.toLocaleUpperCase())
+  );
+  const [searchInput, setSearchInput] = useState(buscar);
+
+  useEffect(() => {
+    if (!covidReducer.length) {
+      dispatch(fetchData());
+    }
+  }, []);
+
+  const filterCountry = (e) => {
+    navigate(e.target.value ? `?buscar=${e.target.value}` : "");
+    setSearchInput(e.target.value);
+  };
+  return (
+    <>
+      <div className="heading">
+        {" "}
+        <h1>EUROPE COUNTRIES' COVID-19 DATA</h1>{" "}
+      </div>
+      <div className="input-box">
+        <input
+          value={searchInput}
+          onChange={filterCountry}
+          className="input"
+          type="text"
+          placeholder="Search European Country ..."
+        />
+      </div>
+      <div className="body">
+        {Europe.map((data) => {
+          const { country, flag } = data;
+
+          return (
+            <Link key={country} to={{ pathname: `/country/${country}` }}>
+              <div className="main-cont" key={country}>
+                <h2 className="country">{country}</h2>
+                <div className="image">
+                  <img className="img" src={flag} alt={country} />
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default Home;
